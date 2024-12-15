c9bbc112621107d0a2f93a4f647861431d179acb
jagadish011 <munavallijagadish@gmail.com>
2024-12-15 08:39:25 +0530

    events reponsive

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -5,8 +5,9 @@ import eduImg2 from "../assets/images/edu-2.png";
 import eduImg3 from "../assets/images/eduction1.png";
 import Footer from "../components/Footer";
 import "aos/dist/aos.css";
-import "../assets/styles/core.css";
 import AOS from "aos";
+import "../pages/styles/education.css";
+// import "../assets/styles/core.css";
 
 function Education() {
   useEffect(() => {
@@ -25,55 +26,40 @@ function Education() {
     },
   };
 
-
   return (
     <>
       <Navbar />
       <div style={{ backgroundColor: "#E8E5E7", overflow: "hidden" }}>
-        <div className="container" style={styles.container}>
-          <div className="row">
-            <div className="col-lg-6 col-md-12 eduction-s1 " style={{ ...styles.content, height: "100%" }}>
-              <h1
-                className="fw-bolder mt-5 custom-text4"
-                style={{
-                  fontSize: "3em",
-                  color: "black",
-                  fontFamily: "Oswald",
-                }}
-              >
-                Education at Kalasindhu
-              </h1>
-              <p
-                className="text-dark mt-5"
-                style={{ fontSize: "20px", fontFamily: "Open Sans" }}
-                data-aos="fade-up"
-                data-aos-easing="linear"
-                data-aos-duration="1000"
-              >
-                At Kalasindhu, a comprehensive and integrated approach to education is offered. Music and Sanskrit are taught along with dance, so students understand the lyrics and meaning of the items they perform.
+        <div className="container">
+          <div className="education">
+            <div className="education-content">
+              <h1 className="education-title">Education at Kalasindhu</h1>
+              <p className="education-description">
+                At Kalasindhu, a comprehensive and integrated approach to
+                education is offered. Music and Sanskrit are taught along with
+                dance, so students understand the lyrics and meaning of the
+                items they perform.
                 <br />
-                <br /> The dedicated space, designed for all ages, sex, skill, fosters creativity in a serene ambiance.
+                <br />
+                The dedicated space, designed for all ages, sex, and skill,
+                fosters creativity in a serene ambiance.
               </p>
-              <a
-                href="https://wa.me/9731098227"
-                className="btn btn-lg btn-dark rounded mt-5"
-                style={{
-                  width: "198px",
-                  background: "#0E0E0E",
-                  fontFamily: "Open Sans",
-                  fontWeight: "bold",
-                  color: "#FCB541",
-                }}
-              >
+              <a href="https://wa.me/9731098227" className="enquire-button">
                 Enquire
               </a>
             </div>
-            <div className="col-lg-6 col-md-12 mt-5 d-flex justify-content-center">
-              <img src={eduImg1} alt="Hero Image" className="img-fluid" />
+            <div className="education-image">
+              <img src={eduImg1} alt="Education Hero" className="img-fluid" />
             </div>
           </div>
         </div>
-        <div style={{ backgroundColor: "#080808", color: "#E8E5E7", overflow: "hidden" }}>
+        <div
+          style={{
+            backgroundColor: "#080808",
+            color: "#E8E5E7",
+            overflow: "hidden",
+          }}
+        >
           <div className="container">
             <div className="row mt-5 mb-4 " style={styles.container}>
               <div className="col-lg-6 col-md-12 text-center">
@@ -85,7 +71,10 @@ function Education() {
                 />
               </div>
               <div className="col-lg-6 col-md-12">
-                <h1 className="text-white fw-bolder custom-text5" style={{ fontSize: "4rem", fontFamily: "Oswald" }}>
+                <h1
+                  className="text-white fw-bolder custom-text5"
+                  style={{ fontSize: "4rem", fontFamily: "Oswald" }}
+                >
                   Curriculum
                 </h1>
                 <ol
@@ -100,16 +89,22 @@ function Education() {
                   }}
                 >
                   <li>
-                    The curriculum includes meticulous instruction in hand gestures, facial expressions, adavus, and a traditional Bharatanatyam repertoire.
+                    The curriculum includes meticulous instruction in hand
+                    gestures, facial expressions, adavus, and a traditional
+                    Bharatanatyam repertoire.
                   </li>
                   <li className="mt-3">
-                    Dance training goes beyond technique, incorporating flexibility and cardio fitness.
+                    Dance training goes beyond technique, incorporating
+                    flexibility and cardio fitness.
                   </li>
                   <li className="mt-3">
-                    Music and Sanskrit are taught from basics along with what is needed for dance.
+                    Music and Sanskrit are taught from basics along with what is
+                    needed for dance.
                   </li>
                   <li className="mt-3">
-                    Advanced training includes rangapravesha, in-depth exploration of Tala and Abhinaya, and opportunities for composing and choreographing dance pieces.
+                    Advanced training includes rangapravesha, in-depth
+                    exploration of Tala and Abhinaya, and opportunities for
+                    composing and choreographing dance pieces.
                   </li>
                 </ol>
                 <a
@@ -132,7 +127,14 @@ function Education() {
         <div className="container" style={styles.container}>
           <div className="row mt-5 mb-4">
             <div className="col-lg-6 col-md-12">
-              <h1 className="fw-bolder mt-5 custom-text5" style={{ fontSize: "60px", color: "black", fontFamily: "Oswald" }}>
+              <h1
+                className="fw-bolder mt-5 custom-text5"
+                style={{
+                  fontSize: "60px",
+                  color: "black",
+                  fontFamily: "Oswald",
+                }}
+              >
                 Added benefits
               </h1>
               <ol
@@ -146,12 +148,27 @@ function Education() {
                 data-aos-easing="linear"
                 data-aos-duration="1000"
               >
-                <li>Students are exposed to diverse dance styles and forms, broadening their artistic horizons.</li>
-                <li className="mt-3">Opportunities for solo or group performances, competitions, and corporate events in and out of state.</li>
-                <li className="mt-3">Advanced students engage in natuvanga and teach juniors.</li>
-                <li className="mt-3">Training includes Rangapravesha and state exams at all levels.</li>
-                <li className="mt-3">Annual workshops with renowned artists enrich the learning experience.</li>
-                <li className="mt-3">Abundant memories and fun throughout the journey.</li>
+                <li>
+                  Students are exposed to diverse dance styles and forms,
+                  broadening their artistic horizons.
+                </li>
+                <li className="mt-3">
+                  Opportunities for solo or group performances, competitions,
+                  and corporate events in and out of state.
+                </li>
+                <li className="mt-3">
+                  Advanced students engage in natuvanga and teach juniors.
+                </li>
+                <li className="mt-3">
+                  Training includes Rangapravesha and state exams at all levels.
+                </li>
+                <li className="mt-3">
+                  Annual workshops with renowned artists enrich the learning
+                  experience.
+                </li>
+                <li className="mt-3">
+                  Abundant memories and fun throughout the journey.
+                </li>
               </ol>
               <a
                 href="https://wa.me/9731098227"
@@ -181,7 +198,15 @@ function Education() {
           <div className="container" style={styles.container}>
             <div className="row mt-5">
               <div className="col-lg-10 col-md-12">
-                <h1 className="fw-bolder custom-text5" style={{ color: "#E8E5E7", fontSize: "60px", fontWeight: "600", fontFamily: "Oswald" }}>
+                <h1
+                  className="fw-bolder custom-text5"
+                  style={{
+                    color: "#E8E5E7",
+                    fontSize: "60px",
+                    fontWeight: "600",
+                    fontFamily: "Oswald",
+                  }}
+                >
                   Admissions and schedule
                 </h1>
                 <p
@@ -195,7 +220,9 @@ function Education() {
                     fontFamily: "Open Sans",
                   }}
                 >
-                  For beginners aged 7 and above, Kalasindhu provides a welcoming entry point. Enrollment is available in May, June, and October.
+                  For beginners aged 7 and above, Kalasindhu provides a
+                  welcoming entry point. Enrollment is available in May, June,
+                  and October.
                 </p>
                 <p
                   data-aos="fade-up"
@@ -208,7 +235,9 @@ function Education() {
                     fontFamily: "Open Sans",
                   }}
                 >
-                  Classes are held on Mondays, Wednesdays, from 5:00 to 8 pm COVERING 4 batches each focusing on different levels of learning.
+                  Classes are held on Mondays, Wednesdays, from 5:00 to 8 pm
+                  COVERING 4 batches each focusing on different levels of
+                  learning.
                 </p>
                 <p
                   data-aos="fade-up"
