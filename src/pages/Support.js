8ed77776e764a221c92e7de2355ec602520083a2
jagadish011 <munavallijagadish@gmail.com>
2024-12-17 19:24:46 +0530

    support page design

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import facebook from "../assets/images/Collobaration1.png"; // Importing the Facebook image
-import "../assets/styles/core.css";
+import facebook from "../assets/images/Collobaration1.png";
+// import "../assets/styles/core.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import AOS from "aos";
@@ -28,14 +28,12 @@ function Support() {
   return (
     <>
       <Navbar />
-      <div>
-        <div className="section-1 box">
-          <div className="hero-overlay text-center d-flex flex-column justify-content-center align-items-center text-white">
+      <div style={{ backgroundColor: "#E8E5E7" }}>
+        <div className="section-1">
+          <div className="support-overlay ">
             <h1
               style={{
-                fontWeight: "600",
                 fontFamily: "Oswald",
-                marginBottom: "10px",
               }}
             >
               Support Us
@@ -46,16 +44,12 @@ function Support() {
           </div>
         </div>
 
-        <section
-          id="support-details"
-          className="p-4"
-          style={{ backgroundColor: "#E8E5E7" }}
-        >
-          <div className="container p-4 mt-5 mb-5">
-            <p className="fs-1 fw-bolder">Support us..</p>
-            <div className="text-start mt-4">
-              <p className="fw-bolder fs-3">By sponsoring us</p>
-              <p className="text-start text-black">
+        <section className="support-details">
+          <div className="support-container">
+            <h1 className="">Support us..</h1>
+            <div className="support-list">
+              <p className="support-list-items">By sponsoring us</p>
+              <p className="">
                 Support our Indian classical dance class by becoming a sponsor
                 of our major festivals, Nirantara Narmada and Navarasa
                 Navaratri. Your sponsorship in money and other kind will help
@@ -65,8 +59,8 @@ function Support() {
               </p>
             </div>
 
-            <div className="text-start mt-3">
-              <p className="fw-bolder fs-3">By Donating</p>
+            <div className="support-list">
+              <p className="support-list-items">By Donating</p>
               <p>
                 Contribute to our dance class and festivals through donations.
                 Your generosity directly supports our mission of providing
@@ -74,8 +68,10 @@ function Support() {
               </p>
             </div>
 
-            <div className="text-start mt-3">
-              <p className="fw-bolder fs-3">By Following us on social media </p>
+            <div className="support-list">
+              <p className="support-list-items">
+                By Following us on social media{" "}
+              </p>
               <p>
                 Stay connected with us on Instagram, Facebook, and YouTube for
                 exclusive updates, behind-the-scenes content, aesthetic dance
@@ -83,58 +79,46 @@ function Support() {
                 wider audience and promote the beauty of Indian classical dance.
               </p>
             </div>
+          </div>
 
-            <div className="row justify-content-center">
-              <div className="col-md-4 col-sm-6">
-                <div className="card text-center">
-                  <div className="card-body">
+          <div className="support-social-card">
+            <div className="support-f-i">
+              <div className="support-facebook">
+                <div className="facebook-container">
+                  <div className="facebook-body text-center">
                     <a
-                      className="text-decoration-none"
                       href="https://www.facebook.com/kalasindhuacademy"
                       target="_blank"
                       rel="noopener noreferrer"
+                      className="text-decoration-none"
                     >
                       <img
                         src={facebook}
                         alt="Facebook"
-                        style={{
-                          width: "100px",
-                          height: "100px",
-                          objectFit: "cover",
-                          borderRadius: "50%",
-                          border: "2px solid #007bff",
-                          padding: "5px",
-                        }}
+                        className="facebook-image"
                       />
                       <p className="mt-2 text-primary">Facebook</p>
                     </a>
                     <a
-                      className="btn btn-primary mt-3"
                       href="https://www.facebook.com/kalasindhuacademy"
                       target="_blank"
                       rel="noopener noreferrer"
+                      className="btn btn-primary mt-3"
                     >
                       Follow Us on Facebook
                     </a>
                   </div>
                 </div>
               </div>
-
-              <div
-                className="elfsight-app-6be1bfd9-94f3-43ec-aa7f-e3cd4ef106e1"
-                data-elfsight-app-lazy
-              ></div>
-              <div className="col-md-4 col-sm-6">
-                <div className="social-icon text-center text-danger">
-                  <a
-                    className="text-decoration-none social-icon text-danger"
-                    href="https://www.youtube.com/@kalasindhuacademyofdancean1324"
-                  >
-                    {/* <i className="bi bi-youtube fs-1"></i>
-                    <p>YouTube</p> */}
-                  </a>
-                </div>
+              <div className="support-instagram">
+                <div
+                  className="elfsight-app-6be1bfd9-94f3-43ec-aa7f-e3cd4ef106e1"
+                  data-elfsight-app-lazy
+                ></div>
               </div>
+            </div>
+
+            <div className="support-youtube">
               <div
                 className="elfsight-app-817dc0c9-2251-4808-88b7-7d28766289b4"
                 data-elfsight-app-lazy
