c30e96b44d1621b8e8f5c6e4a27e91f8e4cc8282
jagadish011 <munavallijagadish@gmail.com>
2024-12-17 22:37:19 +0530

    merging the responsive code

--- src/pages/Festivals.js
+++ src/pages/Festivals.js
@@ -5,9 +5,9 @@ import Navbar from '../components/Navbar'
 const Festivals = () => {
 
    
-    navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position.coords.latitude, position.coords.longitude);
-      });
+    // navigator.geolocation.getCurrentPosition((position) => {
+    //     console.log(position.coords.latitude, position.coords.longitude);
+    //   });
       
   return (
     <>
