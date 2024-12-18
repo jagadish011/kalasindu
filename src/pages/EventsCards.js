71570d9bb79bd0e85870e8f46f778bfbd87fcaea
jagadish011 <munavallijagadish@gmail.com>
2024-12-18 11:27:28 +0530

    home font changes

--- src/pages/EventsCards.js
+++ src/pages/EventsCards.js
@@ -15,7 +15,7 @@ const EventsCard = ({ eventLink, imgSrc, title, artist, date, time, location, pr
           </div>
           <div className="p-3 mb-2 bg-white text-dark rounded-bottom-4">
             <h3 className="fw-bold">{title}</h3>
-            <p className="fw-bold">{artist}</p>
+            <p className="fw-bold" style={{ fontSize: "18px" }}>{artist}</p>
             <div>
               <div className="d-flex justify-content-start">
                 <SlCalender className="mt-2" />
@@ -29,15 +29,15 @@ const EventsCard = ({ eventLink, imgSrc, title, artist, date, time, location, pr
                 <IoLocationOutline className="fs-5 mt-2" />
                 <p className="ms-2 mb-2">{location}</p>
               </div>
-              <div className="d-flex justify-content-between align-item-center">
+              <div className="d-flex justify-content-between align-item-center mt-3">
                 <a
                   href={eventLink}
-                  className="fw-bold"
+                  className="fw-bold fs-5 "
                   style={{ textDecoration: "none", color: "black" }}
                 >
                   more info <GoArrowUpRight />
                 </a>
-                <p className="fw-semibold fs-5">{price}</p>
+                <p className="fw-semibold fs-4">{price}</p>
               </div>
             </div>
           </div>
